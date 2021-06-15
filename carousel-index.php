83afa04a57641b29fdfc0f9c42732661317e3ffc
Leonardo Neves <69824231+LeooNevees@users.noreply.github.com>
2021-06-15 10:42:48 -0300

    Feito alteraçao no carousel

--- carousel-index.php
+++ carousel-index.php
@@ -1,52 +1,44 @@
-<div class="bg-index" style="height: 40%;">
-    <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class="" aria-current="true"></button>
-            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class=""></button>
+<div id="carouselExampleControlsNoTouching" class="carousel slide " data-bs-touch="false" data-bs-interval="false">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="/novoEmpreendimento/img/header1.png" class="d-block w-100" alt="">
         </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><image href="/novoEmpreendimento/img/header1.png" height="100%" width="100%"/></svg>
-                <div class="container">
-                    <div class="carousel-caption text-start">
-                        <!--                                <h1>Promoções do Mês</h1>
-                                                        <p>Como boas-vindas, o Ipeças está disponibilizando várias promoções para você</p>
-                                                        <p><a class="btn btn-lg btn-danger bg-vermelho" href="#" >Conferir</a></p>-->
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item ">
-                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><image href="/novoEmpreendimento/img/header2.png" height="100%" width="100%"/></svg>
-
-                <div class="container">
-                    <div class="carousel-caption">
-                        <!--                                <h1>Acessórios</h1>
-                                                        <p>Diversos tipos de acessórios para seu veículos</p>
-                                                        <p><a class="btn btn-lg btn-danger bg-vermelho" href="#">Conferir</a></p>-->
-                    </div>
-                </div>
-            </div>
-
-            <div class="carousel-item">
-                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><image href="/novoEmpreendimento/img/header3.png" height="100%" width="100%"/></rect></svg>
-
-                <div class="container">
-                    <div class="carousel-caption text-end">
-                        <!--                                <h1>Pagamentos e Entregas</h1>
-                                                        <p>Produtos parcelados até 12x com entregas garantidas até o seu endereço</p>
-                                                        <p><a class="btn btn-lg btn-danger bg-vermelho" href="#">Aqui</a></p>-->
-                    </div>
-                </div>
-            </div>
+        <div class="carousel-item">
+            <img src="/novoEmpreendimento/img/header2.png" class="d-block w-100" alt="">
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-            <span><i class="fas fa-angle-left" aria-hidden="true" style="color: black;"></i></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-            <span class="fas fa-angle-right" aria-hidden="true" style="color: black;"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
+        <div class="carousel-item">
+            <img src="/novoEmpreendimento/img/header3.png" class="d-block w-100" alt="">
+        </div>
     </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
 </div>
+
+<!--OUTRO TIPO DE CAROUSEL -->
+<!-- <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/novoEmpreendimento/img/header1.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="/novoEmpreendimento/img/header2.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="/novoEmpreendimento/img/header3.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div> -->
