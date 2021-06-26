86f02756f3500bf5f65513ea8956f52c011ea0ca
Leonardo Neves <69824231+LeooNevees@users.noreply.github.com>
2021-06-26 14:51:14 -0300

    Feito alteração no single Business

--- classes/SingleBusinessPartner.php
+++ classes/SingleBusinessPartner.php
@@ -2,6 +2,10 @@
 
 include_once '/var/www/html/novoEmpreendimento/classes/repository/RelationshipBusinessPartnerRepository.php';
 include_once '/var/www/html/novoEmpreendimento/classes/repository/BusinessPartnerRepository.php';
+include_once '/var/www/html/novoEmpreendimento/classes/SingleProduct.php';
+include_once '/var/www/html/novoEmpreendimento/classes/Card.php';
+include_once '/var/www/html/novoEmpreendimento/classes/CardProducts.php';
+include_once '/var/www/html/novoEmpreendimento/classes/repository/ProductsRepository.php';
 /*
  * Classe para a base da Página de Vendas de cada item
  * OBS: A princípio foi criado para cada ROW(linha no css) uma function trazendo as informações específicas 
@@ -28,6 +32,26 @@ class SingleBusinessPartner{
 				throw new Exception('Erro ao gerar as descrições do produto'.$idBusiness.'. Por favor recarregue a página');
 			}
 
+			//Produtos Parceiro Negócio
+			$dadosProdutos = array(
+				'id_vendedor' => $this->parceiro,
+				'status' => 'ATIVO'
+			);
+			$retornoBuscaProduto = $this->buscaProdutoParceiro($dadosProdutos);
+			$classeCardProducts = new CardProducts('Produtos ativos no Mercado');
+			$retornoProdutos = $classeCardProducts->gerarDadosEstrutura($retornoBuscaProduto, 6);
+			if($retornoProdutos === false){
+				throw new Exception($classeCardProducts->getMensagem());
+			}
+			$auxRetornoProd = $classeCardProducts->getMensagem();
+			if(!count($auxRetornoProd)){
+				$retProdutos = 'Vendedor não possui mercadoria à venda';
+			}
+			$retProdutos = "</div>"
+							."<h4 class='text-center card-titulo'>Produtos ativos no Mercado</h4>"
+							."<div class='row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-3'>"
+								.implode("\n", $auxRetornoProd)
+							."</div>";
             // $retornoAvaliacao = $this->avaliacaoVendedor($retornoDados);
 			// if($retornoAvaliacao === false || !count($retornoAvaliacao)){
 			// 	throw new Exception('Erro ao gerar as avaliações do produto'.$idBusiness.'. Por favor recarregue a página');
@@ -35,7 +59,7 @@ class SingleBusinessPartner{
 
 			// $conteudo = implode("\n", $retornoDetalhesProduto).implode("\n", $retornoDescricao).implode("\n", $retornoAvaliacao);
 
-			$conteudo = implode("\n", $retornoDescricao);
+			$conteudo = implode("\n", $retornoDescricao).$retProdutos;
 
 			$retorno = "<div class='container'>"
 							.$conteudo
@@ -83,7 +107,7 @@ class SingleBusinessPartner{
 			$tempoCadastrado = ((int) date('Y')) - (int) substr($dados->data_cadastro, 0, 4);
 			$auxCadastro = $tempoCadastrado > 1 ? $tempoCadastrado.' anos' : '1 ano';
 			$foto = isset($dados->foto) ? $dados->foto : '/novoEmpreendimento/img/imagemNotFound.png';
-			$imagem = file_exists($foto) ? $foto : '/novoEmpreendimento/img/imagemNotFound.png';
+			$imagem = file_exists('/var/www/html'.$foto) ? $foto : '/novoEmpreendimento/img/imagemNotFound.png';
 			$localizacao = $dados->cidade.'-'.$dados->uf;
 
 			$array = [];
@@ -92,9 +116,9 @@ class SingleBusinessPartner{
 			$array[] = "<br><div class='row row-cols-1 row-cols-lg-2'>"
 				."<div class='col col-lg-6'>"
 					."<div class='card shadow-sm' style='height:100%;'>"
-						."<div class='card-footer text-muted'>"
+						."<div class='card-footer text-muted' style='height:100%;'>"
 						."<h2 class='cor-letra-titulo text-center'>Dados sobre o vendedor</h2>"
-							."<img src='$imagem' class='img-thumbnail' style='height:100px; width:100px'>"
+							."<img src='$imagem' class='img-thumbnail img-single-business'>"
 							."<div class='card-dados-vendedor'>"
 							."<p class='card-font-valor'>$nomeVendedor</p>"
 							."<p>$auxCadastro vendendo no iPeças</p>"
@@ -104,17 +128,43 @@ class SingleBusinessPartner{
 					."</div>"
 				."</div>";
 
-				$retornoRelacaoParceiro = $this->buscaRelacaoParceiroNegocio($this->parceiro);
-				$classificacao = isset($retornoRelacaoParceiro->classificacao) ? mb_strtolower($retornoRelacaoParceiro->classificacao) : 'bronze';
-				$vendas = isset($retornoRelacaoParceiro->vendas) ? ($retornoRelacaoParceiro->vendas > 1 ? $retornoRelacaoParceiro->vendas.' vendas concretizadas' : $retornoRelacaoParceiro->vendas.' venda concretizada') : 'Nenhuma venda concretizada';
+
+				
+
+				$retVendedor = (new SingleProduct)->buscarVendedor($this->parceiro);
+				$vendas = isset($retVendedor['vendas']) ? ($retVendedor['vendas'] > 1 ? $retVendedor['vendas'].' concretizadas' : $retVendedor['vendas'].' concretizada') : 'Nenhuma venda concretizada';
+				$classificacao = isset($retVendedor['classificacao']) && !empty($retVendedor['classificacao']) ? mb_strtolower($retVendedor['classificacao']) : 'bronze';
+				$titulo = ucfirst($classificacao);
+				$detalheEntrega = isset($retVendedor['detalheEntrega']) && !empty($retVendedor['detalheEntrega'])? $retVendedor['detalheEntrega'] : 'Sem entregas';
+				$classeEntrega = isset($retVendedor['classeEntrega']) && !empty($retVendedor['classeEntrega']) ? $retVendedor['classeEntrega'] : '';
+				$detalheAtendimento = isset($retVendedor['detalheAtendimento']) && !empty($retVendedor['detalheAtendimento']) ? $retVendedor['detalheAtendimento'] : 'Sem atendimentos';
+				$classeAtendimento = isset($retVendedor['classeAtendimento']) && !empty($retVendedor['classeAtendimento']) ? $retVendedor['classeAtendimento'] : '';
+				$classMaiusculo = mb_strtoupper($classificacao);
 
 				$array[] = "<div class='col col-lg-6'>"
 					."<div class='card shadow-sm'>"
-						."<div class='card-footer text-muted'>"
+						."<div class='card-footer text-muted text-center'>"
 							."<h2 class='cor-letra-titulo text-center'>Avaliações</h2>"
-							."<p>Reputação</p>"
-							."<span class='cor-letra-$classificacao'><i class='fas fa-medal'></i> $classificacao</span>"
+							."<span class='cor-letra-$classificacao text-center'><i class='fas fa-medal'></i> $classMaiusculo</span>"
 							."<p>$vendas nos últimos tempos</p>"
+							."<div>"
+								."<table class='table table-borderless text-center'>"
+									."<thead>"
+										."<tr>"
+											."<th class='col-4'><i class='fas fa-shopping-bag fa-2x'></i></th>"
+											."<th class='col-4'><i class='$classeEntrega fas fa-truck fa-2x'></i></th>"
+											."<th class='col-4'><i class='$classeAtendimento far fa-comment-dots fa-2x'></i></th>"
+										."</tr>"
+									."</thead>"
+									."<tbody>"
+										."<tr>"
+											."<td>$vendas</td>"
+											."<td>$detalheEntrega</td>"
+											."<td>$detalheAtendimento</td>"
+										."</tr>"
+									."</tbody>"
+								."</table>"
+							."</div>"
 						."</div>"
 					."</div>"
 				."</div>";
@@ -126,14 +176,18 @@ class SingleBusinessPartner{
 		}
 	}
 
-	private function buscaRelacaoParceiroNegocio($id_pn){
-		try {
-			$retorno = (new RelationshipBusinessPartnerRepository)->getRelationBusiness(array('id_parceiro' => $id_pn));
-			if($retorno === false){
-				throw new Exception('Erro na funcao Busca Relacao Parceiro Negocio na busca do Id '.$id_pn);
+	private function buscaProdutoParceiro($dadosBusca){
+		try {	
+			if(empty($dadosBusca) || !count($dadosBusca)){
+				throw new Exception('Parâmetros inválidos para a função Busca Produto Parceiro');
 			}
-			$retDecod = json_decode($retorno);
-			return $retDecod[0];
+
+			$retorno = (new ProductsRepository)->getProductLimit($dadosBusca, 6);
+			if($retorno === false){
+				throw new Exception('Erro ao fazer a busca dos produtos do Parceiro de Negócio');
+			}
+
+			return $retorno;
 		} catch (Exception $ex) {
 			$this->mensagem = $ex->getMessage();
 			return false;
